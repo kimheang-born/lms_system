c4e6bb15bf8aac4ab785999735323f28a94747a6
kim <kimheang135@gmail.com>
2021-12-27 13:06:20 +0700

    clean code

--- pages/add-contact.vue
+++ pages/add-contact.vue
@@ -42,39 +42,17 @@
 					</v-form>
 				</v-card>
 			</v-col>
-			<!-- <v-col cols="12" md="4">
-				<material-card class="v-card-profile">
-					<v-avatar slot="offset" class="mx-auto d-block elevation-6" size="130">
-						<img
-							src="https://d2naeiwbqsldcl.cloudfront.net/eyJidWNrZXQiOiJ6MXMzIiwia2V5IjoiejFfcHJvZHVjdGlvblwvdXBsb2Fkc1wvaW1hZ2VzXC8yMDIxMDFcL2Y1MGZjMzk2ZDExOWJkMTM4NDc5NGQzYThiN2Q5MzI5LmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjE1MCwiaGVpZ2h0IjoxNTAsImZpdCI6Imluc2lkZSJ9fX0="
-							alt="img"
-						/>
-					</v-avatar>
-					<v-card-text class="text-center">
-						<h6 class="overline mb-3">CEO / CO-FOUNDER</h6>
-
-						<h4 class="font-weight-light">Alec Thompson</h4>
-
-						<p class="font-weight-light">
-							Don't be scared of the truth because we need to restart the human foundation in truth And I
-							love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
-						</p>
-
-						<v-btn color="success">Follow</v-btn>
-					</v-card-text>
-				</material-card>
-			</v-col> -->
 		</v-row>
 	</v-container>
 </template>
 
 <script>
-  	export default {
-		data: () => ({
-			salutations: ['Mr.', 'Ms.', 'Mrs.', 'Oknha', 'H.E', 'Dr.', 'Neak Oknha', 'Dato'],
-			types: ['Personal', 'Business'],
-			khan_provinces: ['Toul Kork', 'Sen Sokh', 'Kratie'],
-			items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-		}),
-	};
+export default {
+	data: () => ({
+		salutations: ['Mr.', 'Ms.', 'Mrs.', 'Oknha', 'H.E', 'Dr.', 'Neak Oknha', 'Dato'],
+		types: ['Personal', 'Business'],
+		khan_provinces: ['Toul Kork', 'Sen Sokh', 'Kratie'],
+		items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+	}),
+};
 </script>
